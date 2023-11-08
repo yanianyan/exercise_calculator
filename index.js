f21bd7a50efb16a509c6c35ba930b3052dc1cb5f
Yanyan <146277878+yanianyan@users.noreply.github.com>
2023-11-08 22:57:46 +0800

    added an a js code

---
+++ index.js
@@ -0,0 +1,9 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+
+buttons.map( button => {
+    button.addEventListener('click',)
+})
+
